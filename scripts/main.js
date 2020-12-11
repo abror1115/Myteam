ec6a98bb96568d7468fa727bd6050b92c9222e9a
unknown <abror7145@gmail.com>
2020-12-11 22:32:21 +0500

    76777

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+var qweertWSX = document.querySelector("#btn-more");
+var wvertWQ = document.querySelector("#item-opened");
+var qweertWS2 = document.querySelector("#btn-more2");
+var wvertWQ2 = document.querySelector("#item-opened2");
+var qweertWS3 = document.querySelector("#btn-more3");
+var wvertWQ3 = document.querySelector("#item-opened3");
+var qweertWS4 = document.querySelector("#btn-more4");
+var wvertWQ4 = document.querySelector("#item-opened4");
+var qweertWS5 = document.querySelector("#btn-more5");
+var wvertWQ5 = document.querySelector("#item-opened5");
+var qweertWS6 = document.querySelector("#btn-more6");
+var wvertWQ6 = document.querySelector("#item-opened6");
+
+
+
+
+qweertWSX.addEventListener("click",function(e){
+  qweertWSX.classList.toggle("opened")
+  wvertWQ.classList.toggle("open")
+});
+qweertWS2.addEventListener("click",function(){
+  qweertWS2.classList.toggle("opened")
+  wvertWQ2.classList.toggle("open")
+
+});
+qweertWS3.addEventListener("click",function(e){
+  qweertWS3.classList.toggle("opened")
+  wvertWQ3.classList.toggle("open")
+
+});
+qweertWS4.addEventListener("click",function(e){
+  qweertWS4.classList.toggle("opened")
+  wvertWQ4.classList.toggle("open")
+
+});
+qweertWS5.addEventListener("click",function(e){
+  qweertWS5.classList.toggle("opened")
+  wvertWQ5.classList.toggle("open")
+
+});
+qweertWS6.addEventListener("click",function(e){
+  qweertWS6.classList.toggle("opened")
+  wvertWQ6.classList.toggle("open")
+});
+
+
+
